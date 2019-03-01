d86b4b93638c886438d101f0f279ef68a2687b60
Bernard Bussy <brbitzbussy@gmail.com>
2019-03-01 20:51:22 +0200

    Or criterion added

---
+++ src/brain/search/criterion/Or.js
@@ -0,0 +1,60 @@
+import Base from 'brain/Base'
+import {Or as OrCriterion} from 'brain/search/criterion/types'
+import {isObject} from 'utilities/type/index'
+
+export default class Or extends Base {
+  static type = OrCriterion
+
+  /**
+   * @type {Or}
+   * @private
+   */
+  _type = Or.type
+
+  /**
+   *  list of criteria to be combined with or
+   * @type {{criteria: Array}}
+   * @private
+   */
+  _value = {
+    criteria: [],
+  }
+
+  constructor(text) {
+    super()
+    try {
+      if (
+          (text !== undefined) &&
+          (
+              (text instanceof Or) ||
+              isObject(text)
+          )
+      ) {
+        this._value.criteria = text.criteria
+      }
+    } catch (e) {
+      throw new Error(`error constructing text criterion object: ${e}`)
+    }
+  }
+
+  get type() {
+    return this._type
+  }
+
+  get value() {
+    return this._value
+  }
+
+  get criteria() {
+    return this._value.criteria
+  }
+
+  set criteria(newVal) {
+    this._value.criteria = newVal
+  }
+
+  get blank() {
+    return this._value.criteria.length === 0
+  }
+
+}
