0d6916a07d010cbfaa0c7e80ff2263004f55baa3
Bernard Bussy <brbitzbussy@gmail.com>
2019-03-21 18:07:27 +0200

    styling fix

--- src/views/dashboard/tracking/live/Live.js
+++ src/views/dashboard/tracking/live/Live.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from 'react'
 // import PropTypes from 'prop-types'
 import {
   withStyles,
@@ -7,37 +7,37 @@ import {
   ExpansionPanelDetails,
   ExpansionPanelSummary,
   Grid
-} from "@material-ui/core";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MultiSelect from "components/multiSelect";
-import { FullPageLoader } from "components/loader/index";
-import { CompanyRecordHandler } from "brain/party/company";
-import { ClientRecordHandler } from "brain/party/client";
-import { TrackingReport } from "brain/report/tracking";
+} from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import MultiSelect from 'components/multiSelect'
+import {FullPageLoader} from 'components/loader/index'
+import {CompanyRecordHandler} from 'brain/party/company'
+import {ClientRecordHandler} from 'brain/party/client'
+import {TrackingReport} from 'brain/report/tracking'
 import MapGL, {
   Marker
   // Popup,
   // NavigationControl,
-} from "react-map-gl";
-import "components/mapbox/Custom.css";
-import { MapPin, MapPinPopup } from "./map";
-import { Reading } from "brain/tracker/reading";
-import { SystemRecordHandler } from "brain/party/system";
+} from 'react-map-gl'
+import 'components/mapbox/Custom.css'
+import {MapPin, MapPinPopup} from './map'
+import {Reading} from 'brain/tracker/reading'
+import {SystemRecordHandler} from 'brain/party/system'
 
 const TOKEN =
-  "pk.eyJ1IjoiaW1yYW5wYXJ1ayIsImEiOiJjanJ5eTRqNzEwem1iM3lwazhmN3R1NWU4In0.FdWdZYUaovv2FY5QcQWVHg";
+    'pk.eyJ1IjoiaW1yYW5wYXJ1ayIsImEiOiJjanJ5eTRqNzEwem1iM3lwazhmN3R1NWU4In0.FdWdZYUaovv2FY5QcQWVHg'
 
 const styles = theme => ({
   root: {
-    height: "100%",
-    display: "grid",
-    gridTemplateRows: "auto 1fr",
-    gridTemplateColumns: "1fr"
+    height: '100%',
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr',
+    gridTemplateColumns: '1fr',
   },
   expanderRoot: {},
   heading: {
     fontSize: theme.typography.pxToRem(15),
-    flexBasis: "33.33%",
+    flexBasis: '33.33%',
     flexShrink: 0
   },
   secondaryHeading: {
@@ -49,29 +49,29 @@ const styles = theme => ({
   selectRoot: { padding: 10 },
   availableRoot: {},
   availableWindow: {
-    backgroundColor: "#f2f2f2",
-    boxShadow: "inset 0 0 4px #000000",
+    backgroundColor: '#f2f2f2',
+    boxShadow: 'inset 0 0 4px #000000',
     height: 120,
     padding: 5,
-    overflow: "auto"
+    overflow: 'auto',
   },
   selectedRoot: {},
   selectedWindow: {
-    backgroundColor: "#f2f2f2",
-    boxShadow: "inset 0 0 4px #000000",
+    backgroundColor: '#f2f2f2',
+    boxShadow: 'inset 0 0 4px #000000',
     height: 120,
     padding: 5,
-    overflow: "auto"
+    overflow: 'auto',
   },
   chip: {},
   chipWrapper: {
     padding: 2
   },
   searchField: {
-    width: "100%"
+    width: '100%',
   },
   map: {}
-});
+})
 
 // const TOKEN = 'pk.eyJ1IjoiaW1yYW5wYXJ1ayIsImEiOiJjanJ5eTRqNzEwem1iM3lwazhmN3R1NWU4In0.FdWdZYUaovv2FY5QcQWVHg'
 
@@ -81,37 +81,37 @@ const styles = theme => ({
  * @returns {string}
  */
 function getRandomColor(exclude = []) {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  let firstTime = true;
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  let firstTime = true
   while (exclude.includes(color) || firstTime) {
-    firstTime = false;
-    color = "#";
+    firstTime = false
+    color = '#'
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 16)]
     }
   }
 
-  return color;
+  return color
 }
 
 const filterPanels = {
   company: 0,
   client: 1
-};
+}
 
 class Live extends Component {
   constructor(props) {
-    super(props);
-    this.loadReport = this.loadReport.bind(this);
-    this.renderFiltersMenu = this.renderFiltersMenu.bind(this);
-    this.handleClientFilterChange = this.handleClientFilterChange.bind(this);
-    this.handleCompanyFilterChange = this.handleCompanyFilterChange.bind(this);
-    this.load = this.load.bind(this);
-    this.updateMapViewport = this.updateMapViewport.bind(this);
-    this.renderDeviceLocations = this.renderDeviceLocations.bind(this);
-    this.getMapDimensions = this.getMapDimensions.bind(this);
-    this.renderMapPipPopup = this.renderMapPipPopup.bind(this);
+    super(props)
+    this.loadReport = this.loadReport.bind(this)
+    this.renderFiltersMenu = this.renderFiltersMenu.bind(this)
+    this.handleClientFilterChange = this.handleClientFilterChange.bind(this)
+    this.handleCompanyFilterChange = this.handleCompanyFilterChange.bind(this)
+    this.load = this.load.bind(this)
+    this.updateMapViewport = this.updateMapViewport.bind(this)
+    this.renderDeviceLocations = this.renderDeviceLocations.bind(this)
+    this.getMapDimensions = this.getMapDimensions.bind(this)
+    this.renderMapPipPopup = this.renderMapPipPopup.bind(this)
     this.state = {
       expanded: null,
       viewport: {
@@ -131,67 +131,68 @@ class Live extends Component {
 
       mapPopUpOpen: false,
       selectedReading: new Reading()
-    };
-    this.systems = [];
-    this.companies = [];
-    this.clients = [];
+    }
+    this.systems = []
+    this.companies = []
+    this.clients = []
 
-    this.systemIdentifiers = [];
-    this.companyIdentifiers = [];
-    this.clientIdentifiers = [];
+    this.systemIdentifiers = []
+    this.companyIdentifiers = []
+    this.clientIdentifiers = []
 
-    this.readingPinColorMap = {};
+    this.readingPinColorMap = {}
 
-    this.loadReportTimeout = () => {};
+    this.loadReportTimeout = () => {
+    }
   }
 
   async load() {
-    this.setState({ loading: true });
+    this.setState({loading: true})
     try {
-      this.systems = (await SystemRecordHandler.Collect()).records;
-      this.systemIdentifiers = this.systems.map(system => system.identifier);
+      this.systems = (await SystemRecordHandler.Collect()).records
+      this.systemIdentifiers = this.systems.map(system => system.identifier)
     } catch (e) {
-      console.error("error collecting system", e);
-      return;
+      console.error('error collecting system', e)
+      return
     }
     try {
-      this.companies = (await CompanyRecordHandler.Collect()).records;
+      this.companies = (await CompanyRecordHandler.Collect()).records
       this.companyIdentifiers = this.companies.map(
         company => company.identifier
-      );
+      )
     } catch (e) {
-      console.error("error collecting companies", e);
-      return;
+      console.error('error collecting companies', e)
+      return
     }
     try {
-      this.clients = (await ClientRecordHandler.Collect()).records;
-      this.clientIdentifiers = this.clients.map(client => client.identifier);
+      this.clients = (await ClientRecordHandler.Collect()).records
+      this.clientIdentifiers = this.clients.map(client => client.identifier)
     } catch (e) {
-      console.error("error collecting clients", e);
-      return;
+      console.error('error collecting clients', e)
+      return
     }
-    this.setState({ loading: false });
+    this.setState({loading: false})
   }
 
   componentDidMount() {
-    this.load().then(() => this.loadReport());
+    this.load().then(() => this.loadReport())
   }
 
   handleChange = panel => (event, expanded) => {
     this.setState({
       expanded: expanded ? panel : false
-    });
-  };
+    })
+  }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    const { expanded: prevExpanded } = prevState;
-    const { expanded } = this.state;
+    const {expanded: prevExpanded} = prevState
+    const {expanded} = this.state
     if (expanded !== prevExpanded && expanded !== null) {
     }
   }
 
   async loadReport() {
-    this.setState({ loading: true });
+    this.setState({loading: true})
     try {
       this.setState({
         readings: (await TrackingReport.Live({
@@ -199,36 +200,36 @@ class Live extends Component {
           clientIdentifiers: this.clientIdentifiers,
           systemIdentifiers: this.systemIdentifiers
         })).readings
-      });
-      let usedColors = Object.values(this.readingPinColorMap);
+      })
+      let usedColors = Object.values(this.readingPinColorMap)
       this.state.readings.forEach(reading => {
         if (this.readingPinColorMap[reading.id] === undefined) {
           // if a color has not yet been assigned for this reading id then
           // assign one now
-          const fill = getRandomColor(usedColors);
-          usedColors.push(fill);
-          this.readingPinColorMap[reading.id] = fill;
+          const fill = getRandomColor(usedColors)
+          usedColors.push(fill)
+          this.readingPinColorMap[reading.id] = fill
         }
-      });
+      })
     } catch (e) {
-      console.error("error loading report", e);
+      console.error('error loading report', e)
     }
-    this.setState({ loading: false });
+    this.setState({loading: false})
   }
 
   handleClientFilterChange(selected) {
-    this.clientIdentifiers = selected.map(client => client.identifier);
-    this.loadReportTimeout = setTimeout(this.loadReport, 500);
+    this.clientIdentifiers = selected.map(client => client.identifier)
+    this.loadReportTimeout = setTimeout(this.loadReport, 500)
   }
 
   handleCompanyFilterChange(selected) {
-    this.companyIdentifiers = selected.map(company => company.identifier);
-    this.loadReportTimeout = setTimeout(this.loadReport, 500);
+    this.companyIdentifiers = selected.map(company => company.identifier)
+    this.loadReportTimeout = setTimeout(this.loadReport, 500)
   }
 
   renderFiltersMenu() {
-    const { classes } = this.props;
-    const { expanded } = this.state;
+    const {classes} = this.props
+    const {expanded} = this.state
 
     return (
       <div className={classes.root}>
@@ -243,14 +244,14 @@ class Live extends Component {
             </Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Grid container direction="column" spacing={8} alignItems="center">
+            <Grid container direction='column' spacing={8} alignItems='center'>
               <Grid item>
                 <MultiSelect
-                  displayAccessor="name"
-                  uniqueIdAccessor="id"
-                  selected={this.companies}
-                  available={[]}
-                  onChange={this.handleCompanyFilterChange}
+                    displayAccessor='name'
+                    uniqueIdAccessor='id'
+                    selected={this.companies}
+                    available={[]}
+                    onChange={this.handleCompanyFilterChange}
                 />
               </Grid>
             </Grid>
@@ -267,22 +268,22 @@ class Live extends Component {
             </Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Grid container direction="column" spacing={8} alignItems="center">
+            <Grid container direction='column' spacing={8} alignItems='center'>
               <Grid item>
                 <MultiSelect
-                  displayAccessor="name"
-                  uniqueIdAccessor="id"
-                  selected={this.clients}
-                  available={[]}
-                  onChange={this.handleClientFilterChange}
+                    displayAccessor='name'
+                    uniqueIdAccessor='id'
+                    selected={this.clients}
+                    available={[]}
+                    onChange={this.handleClientFilterChange}
                 />
               </Grid>
             </Grid>
           </ExpansionPanelDetails>
         </ExpansionPanel>
         <ExpansionPanel
-          expanded={expanded === "panel3"}
-          onChange={this.handleChange("panel3")}
+            expanded={expanded === 'panel3'}
+            onChange={this.handleChange('panel3')}
         >
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
             <Typography className={classes.heading}>Map Settings</Typography>
@@ -298,12 +299,12 @@ class Live extends Component {
           </ExpansionPanelDetails>
         </ExpansionPanel>
       </div>
-    );
+    )
   }
 
   updateMapViewport = viewport => {
-    this.setState({ viewport });
-  };
+    this.setState({viewport})
+  }
 
   getMapDimensions(element) {
     try {
@@ -313,15 +314,15 @@ class Live extends Component {
             width: element.clientWidth,
             height: element.clientHeight
           }
-        });
+        })
       }
     } catch (e) {
-      console.error("error getting map dimensions", e);
+      console.error('error getting map dimensions', e)
     }
   }
 
   renderDeviceLocations() {
-    const { readings } = this.state;
+    const {readings} = this.state
 
     return readings.map((reading, idx) => {
       return (
@@ -336,55 +337,55 @@ class Live extends Component {
               fill: this.readingPinColorMap[reading.id]
             }}
             onClick={() => {
-              console.log("clicked!!");
-              this.setState({ selectedReading: reading });
+              console.log('clicked!!')
+              this.setState({selectedReading: reading})
             }}
           />
         </Marker>
-      );
-    });
+      )
+    })
   }
 
   renderMapPipPopup() {
-    const { selectedReading, mapPopUpOpen } = this.state;
+    const {selectedReading, mapPopUpOpen} = this.state
 
     return (
       <MapPinPopup
-        open={mapPopUpOpen}
-        tipSize={5}
-        anchor="top"
-        longitude={selectedReading.longitude}
-        latitude={selectedReading.latitude}
-        closeOnClick={false}
-        onClose={() => this.setState({ selectedReading: undefined })}
+          open={mapPopUpOpen}
+          tipSize={5}
+          anchor='top'
+          longitude={selectedReading.longitude}
+          latitude={selectedReading.latitude}
+          closeOnClick={false}
+          onClose={() => this.setState({ selectedReading: undefined })}
       />
-    );
+    )
   }
 
   render() {
-    const { classes } = this.props;
-    const { loading, viewport, mapDimensions } = this.state;
+    const {classes} = this.props
+    const {loading, viewport, mapDimensions} = this.state
 
     return (
       <div className={classes.root}>
         <div
           style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
           {this.renderFiltersMenu()}
         </div>
         <div className={classes.map} ref={this.getMapDimensions}>
           <MapGL
-            {...viewport}
-            width={mapDimensions.width}
-            height={mapDimensions.height}
-            mapStyle="mapbox://styles/mapbox/dark-v9"
-            onViewportChange={this.updateMapViewport}
-            mapboxApiAccessToken={TOKEN}
+              {...viewport}
+              width={mapDimensions.width}
+              height={mapDimensions.height}
+              mapStyle='mapbox://styles/mapbox/dark-v9'
+              onViewportChange={this.updateMapViewport}
+              mapboxApiAccessToken={TOKEN}
           >
             {this.renderDeviceLocations()}
             {this.renderMapPipPopup()}
@@ -392,13 +393,13 @@ class Live extends Component {
         </div>
         <FullPageLoader open={loading} />
       </div>
-    );
+    )
   }
 }
 
-Live = withStyles(styles)(Live);
+Live = withStyles(styles)(Live)
 
-Live.propTypes = {};
-Live.defaultProps = {};
+Live.propTypes = {}
+Live.defaultProps = {}
 
-export default Live;
+export default Live
