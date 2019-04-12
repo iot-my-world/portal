38bba1f9362a0eb6394102f087f3c589299b08f1
Bernard Bussy <brbitzbussy@gmail.com>
2019-04-12 09:08:58 +0200

    bep table fix

--- src/views/configuration/user/User.js
+++ src/views/configuration/user/User.js
@@ -140,7 +140,12 @@ class User extends Component {
 
   collectCriteria = []
 
-  collectQuery = new Query()
+  collectQuery = new Query({
+    sortBy: [],
+    order: [],
+    limit: 15,
+    offset: 0,
+  })
 
   entityMap = {
     Company: [],
@@ -840,7 +845,7 @@ class User extends Component {
                     noDataText={'No Users Found'}
                     data={records}
                     onCriteriaQueryChange={this.handleCriteriaQueryChange}
-
+                    initialQuery={this.collectQuery}
                     columns={this.columns}
 
                     getTdProps={(state, rowInfo) => {
