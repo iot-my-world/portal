41ef2778eea39cb9e748fc689cfa5a388332a90e
Bernard Bussy <bernard@andile.io>
2019-05-14 21:06:47 +0200

    fix routing

--- src/views/app/newRoutes.js
+++ src/views/app/newRoutes.js
@@ -1,163 +1,162 @@
 import React from 'react'
 
 // @material-ui/icons
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import Apps from "@material-ui/icons/Apps";
-import GridOn from "@material-ui/icons/GridOn";
-import Place from "@material-ui/icons/Place";
-import WidgetsIcon from "@material-ui/icons/Widgets";
-
+import DashboardIcon from '@material-ui/icons/Dashboard'
+import Apps from '@material-ui/icons/Apps'
+import GridOn from '@material-ui/icons/GridOn'
+import Place from '@material-ui/icons/Place'
+import WidgetsIcon from '@material-ui/icons/Widgets'
 
 const dashRoutes = [
   {
-    path: "/dashboard",
-    name: "Dashboard",
+    path: '/app/dashboard',
+    name: 'Dashboard',
     icon: DashboardIcon,
-    component: () => <div></div>
+    component: () => <div></div>,
   },
   {
     collapse: true,
-    path: "/components",
-    name: "Components",
-    state: "openComponents",
+    path: '/app/components',
+    name: 'Components',
+    state: 'openComponents',
     icon: Apps,
     views: [
       {
-        path: "/components/buttons",
-        name: "Buttons",
-        mini: "B",
-        component: () => <div>Buttons</div>
+        path: '/app/components/buttons',
+        name: 'Buttons',
+        mini: 'B',
+        component: () => <div>Buttons</div>,
       },
       {
-        path: "/components/grid-system",
-        name: "Grid System",
-        mini: "GS",
-        component: () => <div>Grid System</div>
+        path: '/app/components/grid-system',
+        name: 'Grid System',
+        mini: 'GS',
+        component: () => <div>Grid System</div>,
       },
       {
-        path: "/components/panels",
-        name: "Panels",
-        mini: "P",
-        component: () => <div>Panels</div>
+        path: '/app/components/panels',
+        name: 'Panels',
+        mini: 'P',
+        component: () => <div>Panels</div>,
       },
       {
-        path: "/components/sweet-alert",
-        name: "Sweet Alert",
-        mini: "SA",
-        component: () => <div>Sweet Alert</div>
+        path: '/app/components/sweet-alert',
+        name: 'Sweet Alert',
+        mini: 'SA',
+        component: () => <div>Sweet Alert</div>,
       },
       {
-        path: "/components/notifications",
-        name: "Notifications",
-        mini: "N",
-        component: () => <div>Notifications</div>
+        path: '/app/components/notifications',
+        name: 'Notifications',
+        mini: 'N',
+        component: () => <div>Notifications</div>,
       },
       {
-        path: "/components/icons",
-        name: "Icons",
-        mini: "I",
+        path: '/app/components/icons',
+        name: 'Icons',
+        mini: 'I',
         component: () => <div>Icons</div>,
       },
       {
-        path: "/components/typography",
-        name: "Typography",
-        mini: "T",
+        path: '/app/components/typography',
+        name: 'Typography',
+        mini: 'T',
         component: () => <div>Typography</div>,
-      }
-    ]
+      },
+    ],
   },
   {
     collapse: true,
-    path: "/forms",
-    name: "Forms",
-    state: "openForms",
+    path: '/app/forms',
+    name: 'Forms',
+    state: 'openForms',
     icon: GridOn,
     views: [
       {
-        path: "/forms/regular-forms",
-        name: "Regular Forms",
-        mini: "RF",
+        path: '/app/forms/regular-forms',
+        name: 'Regular Forms',
+        mini: 'RF',
         component: () => <div>openForms</div>,
       },
       {
-        path: "/forms/extended-forms",
-        name: "Extended Forms",
-        mini: "EF",
+        path: '/app/forms/extended-forms',
+        name: 'Extended Forms',
+        mini: 'EF',
         component: () => <div>Extended Forms</div>,
       },
       {
-        path: "/forms/validation-forms",
-        name: "Validation Forms",
-        mini: "VF",
+        path: '/app/forms/validation-forms',
+        name: 'Validation Forms',
+        mini: 'VF',
         component: () => <div>Validation Forms</div>,
       },
       {
-        path: "/forms/wizard",
-        name: "Wizard",
-        mini: "W",
+        path: '/app/forms/wizard',
+        name: 'Wizard',
+        mini: 'W',
         component: () => <div>Wizard</div>,
-      }
-    ]
+      },
+    ],
   },
   {
     collapse: true,
-    path: "/tables",
-    name: "Tables",
-    state: "openTables",
+    path: '/app/tables',
+    name: 'Tables',
+    state: 'openTables',
     icon: GridOn,
     views: [
       {
-        path: "/tables/regular-tables",
-        name: "Regular Tables",
-        mini: "RT",
-        component: () => <div>Tables</div>
+        path: '/app/tables/regular-tables',
+        name: 'Regular Tables',
+        mini: 'RT',
+        component: () => <div>Tables</div>,
       },
       {
-        path: "/tables/extended-tables",
-        name: "Extended Tables",
-        mini: "ET",
-        component: () => <div>Extended Tables</div>
+        path: '/app/tables/extended-tables',
+        name: 'Extended Tables',
+        mini: 'ET',
+        component: () => <div>Extended Tables</div>,
       },
       {
-        path: "/tables/react-tables",
-        name: "React Tables",
-        mini: "RT",
-        component: () => <div>React Tables</div>
-      }
-    ]
+        path: '/app/tables/react-tables',
+        name: 'React Tables',
+        mini: 'RT',
+        component: () => <div>React Tables</div>,
+      },
+    ],
   },
   {
     collapse: true,
-    path: "/maps",
-    name: "Maps",
-    state: "openMaps",
+    path: '/app/maps',
+    name: 'Maps',
+    state: 'openMaps',
     icon: Place,
     views: [
       {
-        path: "/maps/google-maps",
-        name: "Google Maps",
-        mini: "GM",
-        component: () => <div>openMaps</div>
+        path: '/app/maps/google-maps',
+        name: 'Google Maps',
+        mini: 'GM',
+        component: () => <div>openMaps</div>,
       },
       {
-        path: "/maps/full-screen-maps",
-        name: "Full Screen Map",
-        mini: "FSM",
-        component: () => <div>Full Screen Map</div>
+        path: '/app/maps/full-screen-maps',
+        name: 'Full Screen Map',
+        mini: 'FSM',
+        component: () => <div>Full Screen Map</div>,
       },
       {
-        path: "/maps/vector-maps",
-        name: "Vector Map",
-        mini: "VM",
-        component: () => <div>Vector Map</div>
-      }
-    ]
+        path: '/app/maps/vector-maps',
+        name: 'Vector Map',
+        mini: 'VM',
+        component: () => <div>Vector Map</div>,
+      },
+    ],
   },
   {
-    path: "/widgets",
-    name: "Widgets",
+    path: '/app/widgets',
+    name: 'Widgets',
     icon: WidgetsIcon,
     component: () => <div>Widgets</div>,
   },
-];
-export default dashRoutes;
+]
+export default dashRoutes
