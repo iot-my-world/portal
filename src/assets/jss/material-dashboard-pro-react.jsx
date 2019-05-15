9cf91c7d6bc181c224278819bcab9ea5e1b84c97
Bernard Bussy <bernard@andile.io>
2019-05-15 21:53:55 +0200

    further simplification

--- src/assets/jss/material-dashboard-pro-react.jsx
+++ src/assets/jss/material-dashboard-pro-react.jsx
@@ -113,35 +113,6 @@ const tooltip = {
   lineBreak: "auto"
 }
 
-const title = {
-  color: "#3C4858",
-  textDecoration: "none",
-  fontWeight: "300",
-  marginTop: "30px",
-  marginBottom: "25px",
-  minHeight: "32px",
-  fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-  "& small": {
-    color: "#777",
-    fontSize: "65%",
-    fontWeight: "400",
-    lineHeight: "1"
-  }
-}
-
-const cardTitle = {
-  ...title,
-  marginTop: "0",
-  marginBottom: "3px",
-  minHeight: "auto",
-  "& a": {
-    ...title,
-    marginTop: ".625rem",
-    marginBottom: "0.75rem",
-    minHeight: "auto"
-  }
-}
-
 export {
   //variables
   drawerWidth,
@@ -161,6 +132,4 @@ export {
   primaryBoxShadow,
   defaultBoxShadow,
   tooltip,
-  title,
-  cardTitle,
 }
