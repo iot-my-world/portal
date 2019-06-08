6503e2ded22a2f303491ccce15573f88cfbb00b6
Bernard Bussy <brbitzbussy@gmail.com>
2019-06-08 10:41:08 +0200

    sf001 detail dialog

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -3,9 +3,17 @@ import {
   FaPlus as AddNewIcon,
   FaSyncAlt as ReloadIcon,
 } from 'react-icons/fa'
+import {
+  MdClear as CancelIcon,
+  MdEdit as EditIcon,
+  MdSave as SaveIcon,
+} from 'react-icons/md'
 
 export {
   ViewDetailsIcon,
   AddNewIcon,
   ReloadIcon,
+  CancelIcon,
+  EditIcon,
+  SaveIcon,
 }
