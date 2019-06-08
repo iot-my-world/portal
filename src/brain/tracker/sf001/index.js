c3b8b9e0c2c5684d43e7efb67f7e3defe237b23f
Bernard Bussy <brbitzbussy@gmail.com>
2019-06-08 09:38:48 +0200

    record collection taking place

---
+++ src/brain/tracker/sf001/index.js
@@ -0,0 +1,7 @@
+import SF001Tracker from './SF001'
+import SF001TrackerRecordHandler from './RecordHandler'
+
+export {
+  SF001Tracker,
+  SF001TrackerRecordHandler,
+}
--- src/views/tracker/sf001/SF001.js
+++ src/views/tracker/sf001/SF001.js
@@ -1,10 +1,93 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core'
+import {
+  SF001Tracker, SF001TrackerRecordHandler,
+} from 'brain/tracker/sf001'
+import PartyHolder from 'brain/party/holder/Holder'
+import Query from 'brain/search/Query'
+import HumanUserLoginClaims from 'brain/security/claims/login/user/human/Login'
 
 const styles = theme => ({})
 
+const states = {
+  nop: 0,
+  itemSelected: 1,
+}
+
+const events = {
+  init: states.nop,
+  selectRow: states.itemSelected,
+}
+
 class SF001 extends Component {
+  state = {
+    activeState: events.init,
+
+    recordCollectionInProgress: false,
+    selectedRowIdx: -1,
+    records: [],
+    totalNoRecords: 0,
+
+    selectedSF001Tracker: new SF001Tracker(),
+  }
+
+  partyHolder = new PartyHolder()
+  collectTimeout = () => {
+  }
+  collectCriteria = []
+  collectQuery = new Query()
+
+  componentDidMount() {
+    this.collect()
+  }
+
+  collect = async () => {
+    const {
+      NotificationFailure,
+      party,
+      claims,
+    } = this.props
+    this.setState({recordCollectionInProgress: true})
+    // perform device collection
+    let collectResponse
+    try {
+      collectResponse = await SF001TrackerRecordHandler.Collect(
+        this.collectCriteria,
+        this.collectQuery,
+      )
+      this.setState({
+        records: collectResponse.records,
+        totalNoRecords: collectResponse.total,
+      })
+    } catch (e) {
+      console.error('Error Fetching SF001 devices', e)
+      NotificationFailure('Error Fetching SF001 devices', e)
+      return
+    }
+
+    try {
+      await this.partyHolder.load(
+        collectResponse.records,
+        'ownerPartyType',
+        'ownerId',
+      )
+      await this.partyHolder.load(
+        collectResponse.records,
+        'assignedPartyType',
+        'assignedId',
+      )
+      this.partyHolder.update(
+        party,
+        claims.partyType,
+      )
+    } catch (e) {
+      console.error('Error Loading Associated Parties', e)
+      NotificationFailure('Error Loading Associated Parties')
+    }
+    this.setState({recordCollectionInProgress: false})
+  }  
+  
   render(){
     return <div>Deivce</div>
   }
@@ -12,7 +95,32 @@ class SF001 extends Component {
 
 SF001 = withStyles(styles)(SF001)
 
-SF001.propTypes = {}
+SF001.propTypes = {
+  /**
+   * Success Action Creator
+   */
+  NotificationSuccess: PropTypes.func.isRequired,
+  /**
+   * Failure Action Creator
+   */
+  NotificationFailure: PropTypes.func.isRequired,
+  /**
+   * Show Global App Loader Action Creator
+   */
+  ShowGlobalLoader: PropTypes.func.isRequired,
+  /**
+   * Hide Global App Loader Action Creator
+   */
+  HideGlobalLoader: PropTypes.func.isRequired,
+  /**
+   * Login claims from redux state
+   */
+  claims: PropTypes.instanceOf(HumanUserLoginClaims),
+  /**
+   * Party from redux state
+   */
+  party: PropTypes.object.isRequired,
+}
 SF001.defaultProps = {}
 
 export default SF001
