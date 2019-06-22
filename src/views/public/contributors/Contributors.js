f08775a6d2cddf33e9c64113efe29386a04bbc40
Bernard Bussy <brbitzbussy@gmail.com>
2019-06-22 23:56:58 +0200

    loading github contributor summary

--- src/views/public/contributors/Contributors.js
+++ src/views/public/contributors/Contributors.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 // import PropTypes from 'prop-types'
-import {withStyles} from '@material-ui/core'
+import {
+  withStyles, Avatar, Tooltip,
+} from '@material-ui/core'
 
 const styles = theme => ({
   root: {
@@ -10,10 +12,65 @@ const styles = theme => ({
     justifyContent: 'center',
     margin: '10px',
   },
+  avatar: {
+    margin: 10,
+    width: 60,
+    height: 60,
+  },
 })
 
+const githubRepos = [
+  'brain',
+  'portal',
+  'surgeon',
+  'chamber',
+  'sigbug',
+]
+
 class Contributors extends Component {
-  render(){
+  state = {
+    loading: false,
+  }
+
+  componentDidMount() {
+    this.load()
+  }
+
+  repoContributorData = {}
+  contributorSummary = {}
+
+  load = async () => {
+    this.setState({loading: true})
+    try {
+      // load all repo data
+      for (let repo of githubRepos) {
+        const fetchResult = await fetch(
+          `https://api.github.com/repos/iot-my-world/${repo}/stats/contributors`,
+          {
+            method: 'GET',
+            headers: new Headers({}),
+            mode: 'cors',
+          },
+        )
+        this.repoContributorData[repo] = await fetchResult.json()
+      }
+      // build contributor summary
+      for (let repo in this.repoContributorData) {
+        for (let contributorData of this.repoContributorData[repo]) {
+          if (!this.contributorSummary.hasOwnProperty(contributorData.author.login)) {
+            this.contributorSummary[contributorData.author.login] = {
+              author: contributorData.author,
+            }
+          }
+        }
+      }
+    } catch (e) {
+      console.error('loading error', e)
+    }
+    this.setState({loading: false})
+  }
+
+  render() {
     const {classes} = this.props
     return (
       <div
@@ -22,7 +79,19 @@ class Contributors extends Component {
           height: 1000,
         }}
       >
-        Contributors!
+        {Object.values(this.contributorSummary).map((summary, idx) => (
+          <div key={idx}>
+            <Tooltip
+              title={summary.author.login}
+            >
+              <Avatar
+                alt={summary.author.login}
+                src={summary.author.avatar_url}
+                className={classes.avatar}
+              />
+            </Tooltip>
+          </div>
+        ))}
       </div>
     )
   }
