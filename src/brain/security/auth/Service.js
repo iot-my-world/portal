806e09bd3fc2aa07d690a53dec00335e0d43b211
Bernard Bussy <brbitzbussy@gmail.com>
2019-06-28 20:51:02 +0200

    remove old login cards

--- src/brain/security/auth/Service.js
+++ src/brain/security/auth/Service.js
@@ -2,7 +2,6 @@ import {jsonRpcRequest} from 'utilities/network'
 
 const Service = {
   async Login(usernameOrEmailAddress, password) {
-    console.log('awe')
     return await jsonRpcRequest({
       method: 'Auth.Login',
       request: {
