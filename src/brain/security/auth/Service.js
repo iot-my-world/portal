ded99496dd8100ec71ac7009465e5b8685004b41
Bernard Bussy <brbitzbussy@gmail.com>
2019-06-28 20:37:16 +0200

    simplify jsonRpcRequest utility function with async await

--- src/brain/security/auth/Service.js
+++ src/brain/security/auth/Service.js
@@ -1,8 +1,9 @@
 import {jsonRpcRequest} from 'utilities/network'
 
 const Service = {
-  Login(usernameOrEmailAddress, password) {
-    return jsonRpcRequest({
+  async Login(usernameOrEmailAddress, password) {
+    console.log('awe')
+    return await jsonRpcRequest({
       method: 'Auth.Login',
       request: {
         usernameOrEmailAddress,
