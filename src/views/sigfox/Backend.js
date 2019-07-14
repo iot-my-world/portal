6a18b28dc379aecc08978159dcab357227d3424d
Bernard Bussy <brbitzbussy@gmail.com>
2019-07-14 14:17:34 +0200

    add sigfox backend component

---
+++ src/views/sigfox/Backend.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  makeStyles,
+} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({}))
+
+function Backend(props) {
+  return (
+    <div>
+      Backend!
+    </div>
+  )
+}
+
+Backend.propTypes = {}
+
+Backend.defaultProps = {}
+
+export default Backend
