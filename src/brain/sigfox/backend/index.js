ecf5a73ef35deab7abd04b19d09223605f0b7558
Bernard Bussy <brbitzbussy@gmail.com>
2019-07-14 16:00:47 +0200

    add index file for sigfox backend api

---
+++ src/brain/sigfox/backend/index.js
@@ -0,0 +1,11 @@
+import Backend from './Backend'
+import BackendRecordHandler from './RecordHandler'
+import BackendValidator from './Validator'
+import BackendAdministrator from './Administrator'
+
+export {
+  Backend,
+  BackendRecordHandler,
+  BackendValidator,
+  BackendAdministrator,
+}
--- src/views/sigfox/BackendManagement.js
+++ src/views/sigfox/BackendManagement.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {
+  Card, CardContent,
   makeStyles,
 } from '@material-ui/core'
+import {
+  Backend,
+} from 'brain/sigfox/backend'
+
+const states = {
+  nop: 0,
+  itemSelected: 1,
+}
+
+const events = {
+  init: states.nop,
+  selectRow: states.itemSelected,
+}
 
 const useStyles = makeStyles(theme => ({}))
 
 function BackendManagement(props) {
+  const [activeState, setActiveState] = useState(states.nop)
+  const [recordCollectionInProgress, setRecordCollectionInProgressState] = useState(false)
+  const [records, setRecords] = useState([])
+  const [selectedBackend, setSelectedBackend] = useState(new Backend())
+
   return (
     <div>
-      Backend!
+      <Card>
+        <CardContent>
+          stuff
+        </CardContent>
+      </Card>
     </div>
   )
 }
