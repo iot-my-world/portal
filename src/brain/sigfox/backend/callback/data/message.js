8fd64ce7de889f0458a25d6bb4f03d3e18600126
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-04 22:02:48 +0200

    add sigfox backend data callback message entity type

---
+++ src/brain/sigfox/backend/callback/data/message.js
@@ -0,0 +1,63 @@
+import Base from 'brain/Base'
+import {isObject} from 'utilities/type/index'
+
+class Message extends Base {
+  /**
+   * @type {string}
+   * @private
+   */
+  _id = ''
+
+  /**
+   * @type {number}
+   * @private
+   */
+  _timeStamp = 0
+
+  /**
+   * @type {string}
+   * @private
+   */
+  _deviceId = ''
+
+  /**
+   * @type {Array}
+   * @private
+   */
+  _data = []
+
+  /**
+   * construct a new Message Object
+   * @param {Message|Object} [message]
+   */
+  constructor(message) {
+    super()
+    if (message !== undefined &&
+      (message instanceof Message || isObject(message))) {
+      try {
+        this._id = message.id
+        this._timeStamp = message.timeStamp
+        this._deviceId = message.deviceId
+        this._data = message.data
+      } catch (e) {
+        throw new Error('error constructing new message object: ' + e)
+      }
+    }
+  }
+
+  get id() {
+    return this._id
+  }
+
+  get timeStamp() {
+    return this._timeStamp
+  }
+
+  get deviceId() {
+    return this._deviceId
+  }
+
+  get data() {
+    return this._data
+  }
+}
